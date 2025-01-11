fbc3b78c6d5bff9e975a2edbd240d8899f8de089
myuon <ioi.joi.koi.loi@gmail.com>
2025-01-11 22:31:47 +0900

    feat: use preact & build web

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
+if (Bun.env.DEV === "true") {
+  await Bun.build({
+    entrypoints: ["./web/main.tsx"],
+    outdir: "./web/dist",
+  });
+}
+
 Bun.serve({
   port: 8080,
   async fetch(request, server) {
@@ -12,11 +19,16 @@ Bun.serve({
     console.log(`GET ${url.pathname}`);
 
     const isIndex = url.pathname === "/" || url.pathname === "";
+    const filePath = `./web${isIndex ? "/index.html" : url.pathname}`;
 
-    return new Response(
-      await Bun.file(`./web${isIndex ? "/index.html" : url.pathname}`).bytes(),
-      {}
-    );
+    const file = Bun.file(filePath);
+    if (await file.exists()) {
+      return new Response(await file.bytes(), {
+        headers: { "content-type": file.type },
+      });
+    } else {
+      return new Response("Not found", { status: 404 });
+    }
   },
 });
 
