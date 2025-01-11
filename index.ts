0a101099caab5daa3f95f4ef81c5ed7ee70486ca
myuon <ioi.joi.koi.loi@gmail.com>
2025-01-11 15:43:25 +0900

    chore: bun server

--- index.ts
+++ index.ts
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+Bun.serve({
+  port: 8080,
+  async fetch(request, server) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/api")) {
+      return new Response("Hello, API!", {
+        headers: { "content-type": "text/plain" },
+      });
+    }
+
+    console.log(`GET ${url.pathname}`);
+
+    const isIndex = url.pathname === "/" || url.pathname === "";
+
+    return new Response(
+      await Bun.file(
+        `./public${isIndex ? "/index.html" : url.pathname}`
+      ).bytes(),
+      {}
+    );
+  },
+});
+
+console.log("Server started at http://localhost:8080");
