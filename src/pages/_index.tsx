33eeaf0b31f922e21bb50ca54de17975c3c71ce4
myuon <ioi.joi.koi.loi@gmail.com>
2025-01-13 02:27:07 +0900

    feat: click to join

--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -1,11 +1,29 @@
-import { Link } from "react-router";
+import { Form, Link, useNavigate } from "react-router";
 
 export const IndexPage = () => {
+  const navigate = useNavigate();
   const roomId = localStorage.getItem("roomId");
 
   return (
-    <div>
-      {roomId ? <Link to={`/rooms/${roomId}`}>RoomId: {roomId}</Link> : null}
+    <div style={{ display: "grid", gap: "16px" }}>
+      <div>
+        {roomId ? <Link to={`/rooms/${roomId}`}>RoomId: {roomId}</Link> : null}
+      </div>
+
+      <Form
+        onSubmit={async (event) => {
+          event.preventDefault();
+
+          const formData = new FormData(event.currentTarget);
+          const roomId = formData.get("roomId") as string;
+          if (!roomId) return;
+
+          await navigate(`/rooms/${roomId}`);
+        }}
+      >
+        <input name="roomId" type="text"></input>
+        <button type="submit">Join</button>
+      </Form>
     </div>
   );
 };
